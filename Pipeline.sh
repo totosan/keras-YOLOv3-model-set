023ec16f0aad68e56feb89282a9bdf422749c510
Thomas Tomow <toto_san@live.com>
2021-05-17 19:09:25 +0000

    added custom config for one lable (yolo3) changed classes to one lable

--- Pipeline.sh
+++ Pipeline.sh
@@ -2,26 +2,30 @@
 set -e #if something goes wrong, stop!
 
 #vars
-model=yolo4_mobilenetv3small
+model=yolo4_efficientnet_lite
 anchor=configs/yolo4_anchors.txt
 class_path=configs/custom_classes.txt
 annotation_file="./vott-csv-export/Tomow-Env-export.csv"
+
+#Get yoloV4 weights
+# wget -O weights/yolov4.weights https://github.com/AlexeyAB/darknet/releases/download/darknet_yolo_v3_optimal/yolov4.weights
+#convert weights, if nesseccary
+# python tools/model_converter/convert.py --yolo4_reorder cfg/yolov4.cfg weights/yolov4.weights weights/yolov4.h5
 
 #start pipeline
 echo ""
 echo "1 Training"
 echo "----------"
-python train.py --gpu_num 1 --batch_size 32 --model_type $model --anchors_path $anchor \
-    --annotation_file $annotation_file --classes_path $class_path
+#rm -r ./logs/000/*
+python train.py --gpu_num 2 --batch_size 32 --val_split 0.2 \
+    --model_type $model --anchors_path $anchor --annotation_file $annotation_file --classes_path $class_path --model_image_size=416x416
 
 echo "2 Dumping Model"
 echo "---------------"
-python yolo.py --gpu_num 1 --model_type=$model --weights_path=logs/000/trained_final.h5 \
-    --anchors_path=$anchor --classes_path=$class_path \
+python yolo.py --gpu_num 1 --model_type=$model --weights_path=logs/000/trained_final.h5 --anchors_path=$anchor --classes_path=$class_path \
     --model_image_size=416x416 --dump_model --output_model_file=$model.h5
     
 echo "3 Evaluation of model"
 echo "---------------------"
-python eval.py --model_path=$model.h5 --anchors_path=$anchor --classes_path=$class_path \
-    --model_image_size=416x416 --eval_type=VOC --iou_threshold=0.5 --conf_threshold=0.001 \
-    --annotation_file=$annotation_file --save_result
+python eval.py --model_path=$model.h5 --anchors_path=$anchor --classes_path=$class_path --model_image_size=416x416 --eval_type=VOC \
+    --iou_threshold=0.5 --conf_threshold=0.001 --annotation_file=$annotation_file --save_result
